b8064f89aea8fa0f167793d51417746c0401d67e
Corder Guy <corder@tamu.edu>
2018-12-02 13:55:26 -0600

    Add lisp solutions to day 2

--- advent.lisp
+++ advent.lisp
@@ -25,6 +25,40 @@
           else do (setf (gethash frequency table) t)
           do (setf frequency (+ frequency change)))))
 
+(defun char-counts (id)
+  (let ((chars (remove-duplicates id)))
+    (loop for char across chars
+          collect (count char id))))
 
-(day01a)
-(day01b)
+(defun day02a ()
+  (let ((ids (read-input "input.02")))
+    (* (list-length (remove-if-not #'(lambda (id) (find 2 (char-counts id))) ids))
+       (list-length (remove-if-not #'(lambda (id) (find 3 (char-counts id))) ids)))))
+
+(defun cartesian-product (first-list second-list)
+  (loop for x in first-list
+        nconc (loop for y in second-list
+                 collect (list x y))))
+
+(defun exactly-one-different (strings)
+  (let ((string1 (car strings))
+        (string2 (cadr strings)))
+    (= 1 (count t (loop for a across string1
+                        for b across string2
+                        collect (not (char= a b)))))))
+
+(defun day02b ()
+  (let* ((ids (read-input "input.02"))
+         (matches (remove-if-not #'exactly-one-different (cartesian-product ids ids)))
+         (match (nth 0 matches))
+         (first-string (nth 0 match))
+         (second-string (nth 1 match)))
+    (coerce (loop for char1 across first-string
+             for char2 across second-string
+             if (char= char1 char2)
+             collect char1)
+            'string)))
+
+
+(day02a)
+(day02b)
