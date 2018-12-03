dad92537e933d987a6280f9c92b4a7a43e1fa6f6
Corder Guy <corder@tamu.edu>
2018-12-02 23:16:50 -0600

    Initial solution for day 3

--- advent.py
+++ advent.py
@@ -42,5 +42,76 @@ def day02b():
             return word1[:diffs[0]] + word1[diffs[0]+1:]
 
 
-print(day02a())
-print(day02b())
+def day03a():
+    claims = read_input('input.03')
+    fabric = []
+    for _ in range(1000):
+        fabric.append([])
+
+    for r in fabric:
+        for _ in range(1000):
+            r.append(0)
+
+    for claim in claims:
+        ident, _, xy, wl = claim.split()
+        x, y = xy.strip(":").split(",")
+        w, l = wl.split("x")
+
+        x = int(x)
+        y = int(y)
+        w = int(w)
+        l = int(l)
+
+        for ix in range(w):
+            for iy in range(l):
+                fabric[x + ix][y + iy] += 1
+
+    total = 0
+    for row in fabric:
+        for col in row:
+            if col > 1:
+                total += 1
+
+    return total
+
+
+def day03b():
+    claims = read_input('input.03')
+    fabric = []
+    for _ in range(1000):
+        fabric.append([])
+
+    for r in fabric:
+        for _ in range(1000):
+            r.append([])
+
+    idents = set()
+
+    for claim in claims:
+        ident, _, xy, wl = claim.split()
+        x, y = xy.strip(":").split(",")
+        w, l = wl.split("x")
+
+        idents.add(ident)
+
+        x = int(x)
+        y = int(y)
+        w = int(w)
+        l = int(l)
+
+        for ix in range(w):
+            for iy in range(l):
+                fabric[x + ix][y + iy].append(ident)
+
+    for row in fabric:
+        for col in row:
+            if len(col) != 1:
+                for ident in col:
+                    idents.discard(ident)
+
+    return idents
+
+
+print()
+print(day03a())
+print(day03b())
