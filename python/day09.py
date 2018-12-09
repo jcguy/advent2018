c0117a5e29d0a7a40e3fa6580c62091673c22c17
Corder Guy <corder.guy@gmail.com>
2018-12-09 02:55:15 -0600

    Speed up both parts significantly, by switching to a deque

--- python/day09.py
+++ python/day09.py
@@ -1,64 +1,23 @@
 from InputReader import read_input
+from collections import deque
 import re
-
-
-class Node:
-    def __init__(self, value, pre=None, nex=None):
-        self.value = value
-        self.pre = pre
-        self.nex = nex
-
-
-class LinkedList:
-    def __init__(self, starting_value):
-        self.node = Node(starting_value)
-        self.node.pre = self.node
-        self.node.nex = self.node
-
-    def cycle(self, n):
-        if n == 0:
-            return
-        if n > 0:
-            for _ in range(n):
-                self.node = self.node.nex
-            return
-        if n < 0:
-            for _ in range(-n):
-                self.node = self.node.pre
-            return
-
-    def insert(self, value):
-        new_node = Node(value, self.node, self.node.nex)
-        self.node.nex.pre = new_node
-        self.node.nex = new_node
-        self.node = new_node
-
-    def delete(self):
-        deleted_node = self.node
-        self.node.nex.pre = self.node.pre
-        self.node.pre.nex = self.node.nex
-        value = self.node.value
-        self.node = self.node.nex
-        del deleted_node
-        return value
 
 
 def solve(num_players, highest_marble):
     scores = [0] * num_players
-    circle = LinkedList(0)
     player = 0
+    circle = deque([0])
 
     for marble in range(1, highest_marble + 1):
-        if marble % 23 == 0:
-            scores[player] += marble
-            circle.cycle(-7)
-            scores[player] += circle.delete()
+        if marble % 23:
+            circle.rotate(-1)
+            circle.append(marble)
         else:
-            circle.cycle(1)
-            circle.insert(marble)
+            circle.rotate(7)
+            scores[player] += circle.pop() + marble
+            circle.rotate(-1)
 
-        player += 1
-        player %= num_players
+        player = (player + 1) % num_players
 
     return max(scores)
 
